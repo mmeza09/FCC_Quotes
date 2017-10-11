cbc22d14dd1f91cfd37eb8e0ea022a2576247228
Miguel Meza <miguel.mezachavez@outlook.com>
2017-10-11 12:31:28 -0500

    Final Project

--- src/js/js.js
+++ src/js/js.js
@@ -1,5 +1,6 @@
 var actualQuote = '';
 var actualAuthor = '';
+var color=['#674BFF','#E8297B','#FF9E3F','#E8DF37','#49FF74'];
 function getQuote (){
     $.ajax({
         headers:{
@@ -10,13 +11,34 @@ function getQuote (){
         type: "GET",
         dataType: "json",
         success: function(data){
+            console.log(typeof(data));
             actualQuote=data.quote;
             actualAuthor=data.author;
-            $("#quote").html(actualQuote);
-            $("#author").html(actualAuthor);
-            $(".twitter-share-button").attr("href","https://twitter.com/intent/tweet?hashtags=quote&related=freecodecamp&text="+'"'+actualQuote+'"'+" "+actualAuthor);
-        }
+        
+            $('.blockquote').animate({
+                opacity:0
+            },500,function(){
+                $(this).animate({
+                    opacity:1
+                },500);
+                $("#quote").html(actualQuote);
+                $("#author").html(actualAuthor);
+                $(".twitter-share-button").attr("href","https://twitter.com/intent/tweet?hashtags=quote&related=freecodecamp&text="+'"'+actualQuote+'"'+" "+actualAuthor);
+            });
+            
+            var colorIndex = Math.round(Math.random() * 4);
+            $('html body').animate({
+                backgroundColor:color[colorIndex],
+                color:color[colorIndex]
+            },1000);
 
+            $('.blockquote-footer').animate({
+                color:color[colorIndex]
+            });
+            $('.btn').animate({
+                backgroundColor:color[colorIndex],
+            });
+        }
     });
 }
 
